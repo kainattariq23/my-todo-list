a0041216d48184508ea70a6d25c740a8401ca077
Kainat Tariq <kainattariq2303@gmail.com>
2026-07-23 15:10:58 +0500

    Fix formatting of addButton event listener

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const taskInput = document.getElementById("taskInput");
 const addButton = document.getElementById("addButton");
 const taskList = document.getElementById("taskList");
 
-addButton.addEventListener("click", function () {
+addButton.addEventListener("click", function () 
+                           
+   {
 
     const taskText = taskInput.value;
 
